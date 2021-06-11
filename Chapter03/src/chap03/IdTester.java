5a0dd48abf6985e89928505e7bf95f6a0f555515
mika <73530023+2everlove@users.noreply.github.com>
2021-06-11 12:12:01 +0900

    class&instance method
    How to call them?
    
    instance method : clsssVariationName.MethodName
    class method : className.methodName

---
+++ Chapter03/src/chap03/IdTester.java
@@ -0,0 +1,29 @@
+package chap03;
+//idを付与するclass
+
+class Id{
+	private static int counter = 0; //合計(total)
+	private int id; //id
+	
+	//コンストラクタ(constructor)
+	public Id() {id=++counter;};
+	
+	//id返還するインスタンス(instance)メソッド(method)
+	public int getId() {return id;}
+	
+	//counterを返還するクラス(class)メソッド(method)
+	public static int getCounter() {return counter;}
+}
+
+public class IdTester {
+
+	public static void main(String[] args) {
+		Id a = new Id(); // id1
+		Id b = new Id(); // id2
+		System.out.println("aのid: "+a.getId());
+		System.out.println("bのid: "+b.getId());
+		
+		System.out.println("付与するidの合計: "+Id.getCounter());
+	}
+
+}
