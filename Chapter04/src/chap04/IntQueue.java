7eca9d8b87e1d52a2aef9008728719b6cb809fc4
mika <73530023+2everlove@users.noreply.github.com>
2021-07-04 18:14:22 +0900

    IntQueue
    Queue

---
+++ Chapter04/src/chap04/IntQueue.java
@@ -0,0 +1,32 @@
+package chap04;
+//Queue type int
+
+public class IntQueue {
+	private int max; //capability of queue
+	private int front; // first cursor of factor
+	private int rear ; //last cursor of factor
+	private int num; //current number of data
+	private int[] que;
+	
+	//exception of execute : empty queue 
+	public class EmptyIntQueueException extends RuntimeException{
+		public EmptyIntQueueException() {}
+	}
+
+	//exception of execute : full queue
+	public class OverflowIntQueueException extends RuntimeException{
+		public OverflowIntQueueException() {}
+	}
+
+	public IntQueue(int capacity) {
+		num = front = rear = 0;
+		max = capacity;
+		try {
+			que = new int[max]; // create array for queue
+		} catch(OutOfMemoryError e) { // can't create!!
+			max = 0;
+		}
+	}
+
+}
+
