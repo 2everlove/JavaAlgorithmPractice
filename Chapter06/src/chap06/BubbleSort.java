4f62897f41f6c5677007eedc4920aa31116f77f7
mika <73530023+2everlove@users.noreply.github.com>
2021-08-07 21:46:51 +0900

    Bubble Sort
    Bubble Sort (Ver_1)
    No : 7
    x[0]: 22
    x[1]: 5
    x[2]: 11
    x[3]: 32
    x[4]: 120
    x[5]: 68
    x[6]: 70
    
    sort acs
    x[0] = 5
    x[1] = 11
    x[2] = 22
    x[3] = 32
    x[4] = 68
    x[5] = 70
    x[6] = 120

---
+++ Chapter06/src/chap06/BubbleSort.java
@@ -0,0 +1,42 @@
+package chap06;
+
+import java.util.Scanner;
+
+public class BubbleSort {
+
+	static void swap(int[] a, int idx1, int idx2) {
+		int t = a[idx1];
+		a[idx1] = a[idx2];
+		a[idx2] = t;
+	}
+	
+	static void bubbleSort(int[] a, int n) {
+		for(int i = 0; i < n - 1; i++)
+			for(int j = n - 1; j > i; j--)
+				if(a[j - 1] > a[j])
+					swap(a, j - 1, j);
+	}
+	
+	public static void main(String[] args) {
+		Scanner stdIn = new Scanner(System.in);
+		
+		System.out.println("Bubble Sort (Ver_1)");
+		System.out.print("No : ");
+		int nx = stdIn.nextInt();
+		int[] x = new int[nx];
+		
+		for(int i = 0; i < nx; i++) {
+			System.out.print("x["+ i + "]: ");
+			x[i] = stdIn.nextInt();
+		}
+		
+		bubbleSort(x, nx);
+		
+		System.out.println("sort acs");
+		for(int i = 0; i < nx; i++)
+			System.out.println("x["+ i + "] = "+ x[i]);
+		
+		stdIn.close();
+	}
+
+}
