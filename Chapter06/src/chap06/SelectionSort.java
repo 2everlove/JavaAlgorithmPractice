581aabb72f38d89d2b2e8be8260fe087685ed0e6
mika <73530023+2everlove@users.noreply.github.com>
2021-08-10 23:15:21 +0900

    Stratight Selection Sort
    Selection Sort
    No : 5
    x[0]: 3
    x[1]: 4
    x[2]: 2
    x[3]: 3
    x[4]: 1
    
    sort acs
    x[0] = 1
    x[1] = 2
    x[2] = 3
    x[3] = 3
    x[4] = 4

---
+++ Chapter06/src/chap06/SelectionSort.java
@@ -0,0 +1,45 @@
+package chap06;
+
+import java.util.Scanner;
+
+public class SelectionSort {
+
+	static void swap(int[] a, int idx1, int idx2) {
+		int t = a[idx1];
+		a[idx1] = a[idx2];
+		a[idx2] = t;
+	}
+	
+	static void selectionSort(int[] a, int n) {
+		for(int i = 0; i < n - 1; i++){
+			int min = i;
+			for(int j = i + 1; j < n; j++)
+				if(a[j] < a[min])
+					min = j;
+			swap(a, i, min);
+		}
+	}
+	
+	public static void main(String[] args) {
+		Scanner stdIn = new Scanner(System.in);
+		
+		System.out.println("Bubble Sort (Ver_1)");
+		System.out.print("No : ");
+		int nx = stdIn.nextInt();
+		int[] x = new int[nx];
+		
+		for(int i = 0; i < nx; i++) {
+			System.out.print("x["+ i + "]: ");
+			x[i] = stdIn.nextInt();
+		}
+		
+		selectionSort(x, nx);
+		
+		System.out.println("sort acs");
+		for(int i = 0; i < nx; i++)
+			System.out.println("x["+ i + "] = "+ x[i]);
+		
+		stdIn.close();
+	}
+
+}
