315ff20e977385c72d45b37332c624bbbf17a1ac
mika <73530023+2everlove@users.noreply.github.com>
2021-10-11 18:53:15 +0900

    JavaConnectMySQL
    1. paste connector in Lib folder
    2. Create objcet (Connection, Statement, ResultSet)
    3. Create database, table on MySQL
    4. test

---
+++ JavaWithMySQL/src/ch2/Main.java
@@ -0,0 +1,10 @@
+package ch2;
+
+public class Main {
+
+	public static void main(String[] args) {
+		DBConnection connection = new DBConnection();
+		System.out.println("관리자 여부 : " + connection.isAdmin("admin", "admin"));
+	}
+
+}
